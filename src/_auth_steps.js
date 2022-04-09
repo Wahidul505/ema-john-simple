2e26465b51348d5e8582669621dfb347a6ca5823
Wahidul30 <wahidul505@gmail.com>
2022-04-10 02:49:51 +0600

    added sign in funtionalities

--- src/_auth_steps.js
+++ src/_auth_steps.js
@@ -1,3 +1,18 @@
 /* 
 
+
+1. create-react-app
+2. Create firebase app with the same name and register it.
+3. Go to firebase app > project > build > authentication > enable the sign in method.
+4. npm-install-firebase
+5. Then setup the configuration in firebase.init.js
+6. In firebase.init.js, import {getAuth} from 'firebase/auth'; & set const auth = getAuth(app) & export default auth;
+7. Create LogIn and SignUp component and setup the routes.
+8. npm install --save react-firebase-hooks 
+9. Attache event handler to get email, password and confirm password from the input field of signup component.
+10. Use useCreateUserWithEmailAndPassword hooks from-react-firebase hooks and get the user from it.
+11. 
+
+
+
  */
--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,29 +1,57 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './LogIn.css';
 
 const LogIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const handleGetEmail = e => {
+        setEmail(e.target.value);
+    };
+    const handleGetPassword = e => {
+        setPassword(e.target.value);
+    };
+    const handleLogIn = e => {
+        e.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    }
+    if(user){
+        navigate('/shop');
+    }
     return (
         <div className='form-container'>
-               <h1 className='form-title'>Login</h1> 
-               <form>
-                    <div className="input-group">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" name="email" required/>
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" required/>
-                    </div>
-                    <input className='submit-btn' type="submit" value="Login" />
-               </form>
-               <p>New to Ema-John? <Link className='form-link' to='/signup'>Create New Account</Link></p>
-               <div className='or'>
-                   <p className='line'></p>
-                   <p>or</p>
-                   <p className='line'></p>
-               </div>
-               <button className='google-signIn'>Continue with Google</button>
+            <h1 className='form-title'>Login</h1>
+            <form onSubmit={handleLogIn}>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input onBlur={handleGetEmail} type="email" name="email" required />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input onBlur={handleGetPassword} type="password" name="password" required />
+                </div>
+                {
+                    loading && <p>Loading...</p>
+                }
+                <p style={{color: 'red'}}>{error?.message}</p>
+                <input className='submit-btn' type="submit" value="Login" />
+            </form>
+            <p>New to Ema-John? <Link className='form-link' to='/signup'>Create New Account</Link></p>
+            <div className='or'>
+                <p className='line'></p>
+                <p>or</p>
+                <p className='line'></p>
+            </div>
+            <button className='google-signIn'>Continue with Google</button>
         </div>
     );
 };
